80911de05f48e9d2748c697a349a1a942cec05af
CarineT-dev <74300213+CarineT-dev@users.noreply.github.com>
2021-01-20 07:52:27 +0100

    création du package dao et de la classe MaConnexionBDD

---
+++ src/java/dao/MaConnexionBDD.java
@@ -0,0 +1,83 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ Auteur: Djouela
+ Date de création: 20/01/2021
+ 
+ */
+public class MaConnexionBDD {
+
+    private String user;
+    private String mdp;
+    private String url;
+
+    public MaConnexionBDD() {
+        user = "root";
+        mdp = "root";
+        url = "jdbc:mysql://localhost:3306/top_gaming?zeroDateTimeBehavior=convertToNull&serverTimezone=UTC&useSSL=false";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MaConnexionBDD.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(MaConnexionBDD.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(MaConnexionBDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public MaConnexionBDD(String user, String mdp, String url) {
+        this.user = user;
+        this.mdp = mdp;
+        this.url = url;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MaConnexionBDD.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(MaConnexionBDD.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(MaConnexionBDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    
+    public Connection getConnection() throws SQLException{
+        Connection cnn = DriverManager.getConnection(user, mdp, url);
+        return cnn;
+        
+    }
+
+}

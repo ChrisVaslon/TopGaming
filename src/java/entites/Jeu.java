ce1bbaea233fa6b67f1c027b11477251b95026cd
CarineT-dev <74300213+CarineT-dev@users.noreply.github.com>
2021-01-20 11:21:45 +0100

    création entités Categorie et TVA

---
+++ src/java/entites/Jeu.java
@@ -0,0 +1,13 @@
+
+package entites;
+
+
+public class Jeu {
+    private int id;
+    private String nom;
+    private float prixHT;
+    private String description;
+    private String imgage; // nom de la photo
+    private TVA tva;
+    private Categorie categorie;
+}

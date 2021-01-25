6c0ebed019e984cd8b8eadbf0543441eb4ea1883
CarineT-dev <74300213+CarineT-dev@users.noreply.github.com>
2021-01-25 10:54:06 +0100

    modif

--- src/java/dao/MembreDao.java
+++ src/java/dao/MembreDao.java
@@ -25,7 +25,7 @@ public class MembreDao {
     
     public void InsertMembre(String pseudo, String nom, String prenom, Date dateCreationProfil, Date dateNaissance, String mail, String mdp, String rue, String ville,String cp, int tel) throws SQLException{
         try(Connection cnn = McBDD.getConnection();){
-            String sql = "INSERT INTO membre(membre_pseudo, membre_nom, membre_prenom, membre_dateCreationProfil, membre_dateNaissance, membre_mail, membre_mdp, membre_rue, membre_ville, membre_cp, membre_tel, membre_points, Id_grade, role_id) VALUES(?, ?, ?, ?, ?, ?, md5 (?), ?, ?, ?, ?, 10, 1, 1) ";
+            String sql = "INSERT INTO membre(membre_pseudo, membre_nom, membre_prenom, membre_dateCreationProfil, membre_dateNaissance, membre_mail, membre_mdp, membre_rue, membre_ville, membre_cp, membre_tel, membre_points, grade_id, role_id) VALUES(?, ?, ?, ?, ?, ?, md5 (?), ?, ?, ?, ?, 10, 1, 1) ";
                   
             PreparedStatement pstm = cnn.prepareStatement(sql);
             pstm.setString(1, pseudo);
